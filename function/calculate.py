45deb6ecbb07c76eb19c4ac0798bc713460fe409
yuii-code <75972988+yuii-code@users.noreply.github.com>
2021-02-20 12:45:57 +0700

    Delete function directory

--- function/calculate.py
+++
@@ -1,9 +0,0 @@
-import calculate
-
-number = 10
-number = 2
-
-print(calculate.add(10, 2))
-print(calculate.subtract(10, 2))
-print(calculate.multiplied(10, 2))
-print(calculate.divide(10, 2))
--- function/variable.py
+++
@@ -1,2 +0,0 @@
-person = {"name": "yui", "age": 21, "country": "bangkok"}
-person1 =  {"name": "o", "age": 20, "country": "bangkok"}
